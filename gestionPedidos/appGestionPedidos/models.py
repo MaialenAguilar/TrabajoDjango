a0e5dca7d9653e4804614b4ac88ded62d6ab79ab
Unknown <alvaro.husillos@opendeusto.es>
2020-04-02 18:40:48 +0200

    Modelos creados y migrados correctamente

--- gestionPedidos/appGestionPedidos/models.py
+++ gestionPedidos/appGestionPedidos/models.py
@@ -2,22 +2,43 @@ from django.db import models
 
 # Create your models here.
 class Cliente(models.Model):
-    cif = models.Charfield(max_lengh=12)
+    cif = models.CharField(max_length=10)
     nombre_empresa = models.CharField(max_length=80)
-    direccion = models.Charfield(max_length=120)
+    direccion = models.CharField(max_length=120)
     codigo_postal = models.IntegerField()
     Localidad = models.CharField(max_length=120)
     Provincia = models.CharField(max_length=60)
     telefono = models.IntegerField()
     email = models.EmailField()
-    persona_contacto = models.CharField(120)
+    persona_contacto = models.CharField(max_length=120)
 
 class Componente(models.Model):
-    codigo_referencia = models.IntegerField()
+    codigo_referencia = models.CharField(max_length=10)
     nombre = models.CharField(max_length=120)
     marca = models.CharField(max_length=80)
 
-class Categoría(models.Model):
+class Categoria(models.Model):
     nombre = models.CharField(max_length=60)
+
+class Producto(models.Model):
+    referencia = models.CharField(max_length=10)
+    precio = models.FloatField()
+    nombre = models.CharField(max_length=50)
+    descripcion = models.TextField()
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    componentes = models.ManyToManyField(Componente)
+
+class Pedido(models.Model):
+    identificador = models.CharField(max_length=10)
+    fecha_pedido = models.DateField()
+    entregado =models.BooleanField(default=False)
+    fecha_entrega = models.DateField()
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    producto = models.ManyToManyField(Producto)
+    cantidad = models.IntegerField()
+    base_imponible = models.FloatField()
+    iva = models.IntegerField(default=21)
+    precio_total = models.FloatField()
+
     
 
