b8bd26d16e2e04519e335741f252d80ee05045f3
Unknown <alvaro.husillos@opendeusto.es>
2020-04-02 11:50:29 +0200

    Nuevo modelos

--- gestionPedidos/appGestionPedidos/models.py
+++ gestionPedidos/appGestionPedidos/models.py
@@ -5,4 +5,19 @@ class Cliente(models.Model):
     cif = models.Charfield(max_lengh=12)
     nombre_empresa = models.CharField(max_length=80)
     direccion = models.Charfield(max_length=120)
+    codigo_postal = models.IntegerField()
+    Localidad = models.CharField(max_length=120)
+    Provincia = models.CharField(max_length=60)
+    telefono = models.IntegerField()
+    email = models.EmailField()
+    persona_contacto = models.CharField(120)
+
+class Componente(models.Model):
+    codigo_referencia = models.IntegerField()
+    nombre = models.CharField(max_length=120)
+    marca = models.CharField(max_length=80)
+
+class Categoría(models.Model):
+    nombre = models.CharField(max_length=60)
+    
 
