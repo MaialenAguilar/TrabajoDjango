8ec98565fc3d7f18b06b3ff11989ad5c92738c47
natxo <ignacio.chapero@opendeusto.es>
2020-04-09 18:58:47 +0200

    liastado_clientes_view

--- gestionPedidos/appGestionPedidos/views.py
+++ gestionPedidos/appGestionPedidos/views.py
@@ -1,3 +1,15 @@
+from .models import Cliente
+
 from django.shortcuts import render
 
-# Create your views here.
+# Devuelve el listado de clientes
+def index(request):
+    clientes = Cliente.objects.order_by('nombre_empresa')
+    output = ', '.join([d.nombre_empresa for d in Cliente])
+    return HttpResponse(output)
+
+#devuelve los datos de un cliente
+def detail(request, cliente_id):
+	clientes = Clienteo.objects.get(pk=cliente_id)
+	output = ', '.join([str(cliente_id), clientes.nombre_empresa, str(cliente.telefono)])
+	return HttpResponse(output)
