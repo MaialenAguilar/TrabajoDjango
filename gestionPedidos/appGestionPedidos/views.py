c68f9e32926a0fe61846d846dba7c65032b10bd1
natxo <ignacio.chapero@opendeusto.es>
2020-04-09 19:29:23 +0200

    vamosssss

--- gestionPedidos/appGestionPedidos/views.py
+++ gestionPedidos/appGestionPedidos/views.py
@@ -1,15 +1,15 @@
 from .models import Cliente
-
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Devuelve el listado de clientes
 def index(request):
     clientes = Cliente.objects.order_by('nombre_empresa')
-    output = ', '.join([d.nombre_empresa for d in Cliente])
+    output = ', '.join([d.nombre_empresa for d in clientes])
     return HttpResponse(output)
 
 #devuelve los datos de un cliente
-def detail(request, cliente_id):
-	clientes = Clienteo.objects.get(pk=cliente_id)
-	output = ', '.join([str(cliente_id), clientes.nombre_empresa, str(cliente.telefono)])
+def detalle(request, clientes_id):
+	clientes = Cliente.objects.get(pk=clientes_id)
+	output = ', '.join([str(cliente_id), Cliente.nombre_empresa, str(Cliente.telefono),str(persona_contacto)])
 	return HttpResponse(output)
