c3e7b29754b99a91fc3e1561868de9f77dc8e806
natxo <ignacio.chapero@opendeusto.es>
2020-04-09 19:29:23 +0200

    09042020_20_00

--- gestionPedidos/appGestionPedidos/urls.py
+++ gestionPedidos/appGestionPedidos/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
- from . import views
+from . import views
 
- urlpatterns = [
+urlpatterns = [
      path('', views.index, name='index'),
+     path('clientes/<int:cliente_id>/', views.detalle(), name='detalle'),
  ]
