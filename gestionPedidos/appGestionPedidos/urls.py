557b9a22cb1614e3e9ec16dc1de5fc81e44443f0
Industria 4.0 <aguilar.maialen@gmail.com>
2020-04-11 14:58:32 +0200

    DetailView y ListView

--- gestionPedidos/appGestionPedidos/urls.py
+++ gestionPedidos/appGestionPedidos/urls.py
@@ -7,13 +7,20 @@ urlpatterns = [
     path('', views.ClientesListView.as_view(), name='clientes'),
    # path('clientes/<int:cliente_id>/', views.detalle_cliente, name='detalle_cliente'),
     path('clientes/<int:pk>/', views.Detalle_ClienteDetailView.as_view(), name='detalle_cliente'),
-    path('componentes/', views.componentes, name='componentes'),
-    path('componentes/<int:componente_id>/', views.detalle_componentes, name='detalle_componentes'),
-    path('categorias/', views.categorias, name='categorias'),
-    path('productos/', views.productos, name='productos'),
-    path('productos/<int:producto_id>/', views.detalle_productos, name='detalle_productos'),
-    path('pedidos/', views.pedidos, name='pedidos'),
-    path('pedidos/<int:pedido_id>/', views.detalle_pedidos, name='detalle_pedidos')
+   # path('componentes/', views.componentes, name='componentes'),
+    path('componentes/', views.ComponentesListView.as_view(), name='componentes'),
+   # path('componentes/<int:componente_id>/', views.detalle_componentes, name='detalle_componentes'),
+    path('componentes/<int:pk>/', views.Detalle_CompoentesDetailView.as_view(), name='detalle_componentes'),
+   # path('categorias/', views.categorias, name='categorias'),
+    path('categorias/', views.CategoriasListView.as_view(), name='categorias'),
+   # path('productos/', views.productos, name='productos'),
+    path('productos/', views.ProductosListView.as_view(), name='productos'),
+   # path('productos/<int:producto_id>/', views.detalle_productos, name='detalle_productos'),
+     path('productos/<int:pk>/', views.Detalle_ProductosDetailView.as_view(), name='detalle_productos'),
+   # path('pedidos/', views.pedidos, name='pedidos'),
+    path('pedidos/', views.PedidosListView.as_view(), name='pedidos'),
+   # path('pedidos/<int:pedido_id>/', views.detalle_pedidos, name='detalle_pedidos')
+    path('pedidos/<int:pk>/', views.Detalle_PedidosDetailView.as_view(), name='detalle_pedidos')
 
 
 ]
--- gestionPedidos/appGestionPedidos/views.py
+++ gestionPedidos/appGestionPedidos/views.py
@@ -4,11 +4,13 @@ from django.shortcuts import get_object_or_404, get_list_or_404
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 
-# Devuelve el listado de clientes
+# DEVUELVE EL LISTADO DE CLIENTES
+
 #def clientes(request):
    #clientes = get_list_or_404(Cliente.objects.order_by('nombre_empresa'))
    #context = {'lista_clientes': clientes}
    #return render(request,'clientes.html',context)
+
 class ClientesListView(ListView):
     model=Cliente
     template_name ='clientes.html'
@@ -23,7 +25,7 @@ class ClientesListView(ListView):
 
 #DEVUELVE DATOS DE UN CLIENTE
 
-#cliente = get_object_or_404(Cliente.objects.get(pk=cliente_id))
+    #cliente = get_object_or_404(Cliente.objects.get(pk=cliente_id))
 	#output = ', '.join([str(cliente.id) , cliente.nombre_empresa, str(cliente.telefono), cliente.persona_contacto])
 	#return HttpResponse(output)
 
@@ -32,6 +34,7 @@ class ClientesListView(ListView):
    #cliente = get_object_or_404(Cliente, pk=cliente_id)
    #context = {'cliente': cliente}
    #return render(request,'Datos_Cliente.html',context)
+
 class Detalle_ClienteDetailView(DetailView):
     model = Cliente
     template_name ='Datos_Cliente.html'
@@ -41,62 +44,147 @@ class Detalle_ClienteDetailView(DetailView):
         context['Titulo_pagina']='Datos de cliente'
         return context
 
-# Devuelve el listado de componentes
-def componentes(request):
-    componentes = get_list_or_404(Componente.objects.order_by('nombre'))
-    #output = ', '.join([d.nombre for d in componentes])
+# DEVUELVE LISTADO DE COMPONENTES
+
+ #output = ', '.join([d.nombre for d in componentes])
     #return HttpResponse(output)
-    context={'lista_componentes': componentes}
-    return render(request, 'componentes.html' , context)
 
-#devuelve los datos de un componente
+#def componentes(request):
+   #componentes = get_list_or_404(Componente.objects.order_by('nombre'))
+   #context={'lista_componentes': componentes}
+   #return render(request, 'componentes.html' , context)
 
-def detalle_componentes(request, componente_id):
-	#componente = Componente.objects.get(pk=componente_id)
+class ComponentesListView(ListView):
+    model = Componente
+    template_name = 'componentes.html'
+    queryset = Componente.objects.order_by('nombre')
+    context_object_name ='lista_componentes'
+
+    def get_context_data(self, **kwargs):
+        context= super(ComponentesListView, self).get_context_data(**kwargs)
+        context ['Titulo_pagina']='Listado de componentes'
+        return context
+
+
+#DEVUELVE LOS DATOS DE UN COMPONENTE
+
+    #componente = Componente.objects.get(pk=componente_id)
 	#output = ', '.join([componente.codigo_referencia, componente.nombre, componente.marca])
 	#return HttpResponse(output)
-    componente= get_object_or_404(Componente, pk= componente_id)
-    context = {'componente':componente}
-    return render(request, 'Datos_Componente.html',context)
 
-# Devuelve el listado de categorias
-def categorias(request):
-    categorias = get_list_or_404(Categoria.objects.order_by('nombre'))
+#def detalle_componentes(request, componente_id):
+    #componente= get_object_or_404(Componente, pk= componente_id)
+    #context = {'componente':componente}
+    #return render(request, 'Datos_Componente.html',context)
+
+class Detalle_CompoentesDetailView(DetailView):
+    model = Componente
+    template_name = 'Datos_Componente.html'
+
+    def get_context_data(self, **kwargs):
+        context= super(Detalle_CompoentesDetailView, self).get_context_data(**kwargs)
+        context['Titulo_pagina']= 'Datos del componente'
+        return context
+
+# DEVUELVE EL LISTADO DE CATEGORIAS
+
     #output = ', '.join([d.nombre for d in categorias])
     #return HttpResponse(output)
-    context= {'lista_categorias':categorias}
-    return render(request, 'categorias.html',context)
 
-# Devuelve el listado de productos
-def productos(request):
-    productos = get_list_or_404(Producto.objects.order_by('nombre'))
+#def categorias(request):
+    #categorias = get_list_or_404(Categoria.objects.order_by('nombre'))
+    #context= {'lista_categorias':categorias}
+    #return render(request, 'categorias.html',context)
+
+class CategoriasListView(ListView):
+    model = Categoria
+    template_name ='categorias.html'
+    queryset = Categoria.objects.order_by('nombre')
+    context_object_name ='lista_categorias'
+
+    def get_context_data(self, **kwargs):
+        context= super(CategoriasListView, self).get_context_data(**kwargs)
+        context['Titulo_pagina']='Listado de categorias'
+        return context
+
+# DEVUELVE EL LISTADO DE PRODUCTOS
+
     #output = ', '.join([d.nombre for d in productos])
     #return HttpResponse(output)
-    context= {'lista_productos':productos}
-    return render(request, 'productos.html',context)
 
-#devuelve los datos de cada producto
-def detalle_productos(request, producto_id):
-	#producto =  Producto.objects.get(pk=producto_id)
+#def productos(request):
+    #productos = get_list_or_404(Producto.objects.order_by('nombre'))
+    #context= {'lista_productos':productos}
+    #return render(request, 'productos.html',context)
+
+class ProductosListView(ListView):
+    model = Producto
+    template_name ='productos.html'
+    queryset = Producto.objects.order_by('nombre')
+    context_object_name ='lista_productos'
+
+    def get_context_data(self, **kwargs):
+        context= super(ProductosListView, self).get_context_data(**kwargs)
+        context['Titulo_pagina']='Listado de productos'
+        return context
+
+#DEVUELVE LOS DATOS DE CADA PRODUCTO
+
+#producto =  Producto.objects.get(pk=producto_id)
 	#output = ', '.join([producto.nombre, producto.descripcion, str(producto.precio)])
 	#return HttpResponse(output)
-    producto= get_object_or_404(Producto, pk= producto_id)
-    context= {'producto': producto}
-    return render(request, 'datos_producto.html', context)
 
-#devuelve lista de pedidos
-def pedidos(request):
-    pedidos = get_list_or_404(Pedido.objects.order_by('entregado'))
+#def detalle_productos(request, producto_id):
+    #producto= get_object_or_404(Producto, pk= producto_id)
+    #context= {'producto': producto}
+    #return render(request, 'datos_producto.html', context)
+
+class Detalle_ProductosDetailView(DetailView):
+    model = Producto
+    template_name = 'Datos_Producto.html'
+
+    def get_context_data(self, **kwargs):
+        context= super(Detalle_ProductosDetailView, self).get_context_data(**kwargs)
+        context['Titulo_pagina']= 'Datos del producto'
+        return context
+
+#DEVUELVE LISTA DE PEDIDOS
+
     #output = ', '.join([d.nombre for d in productos])
     #return HttpResponse(output)
-    context= {'lista_pedidos':pedidos}
-    return render(request, 'pedidos.html',context)
 
-#devuelve los datos de cada pedido
-def detalle_pedidos(request, pedido_id):
-	#pedidos = Pedido.objects.get(pk=pedido_id)
+#def pedidos(request):
+    #pedidos = get_list_or_404(Pedido.objects.order_by('entregado'))
+    #context= {'lista_pedidos':pedidos}
+    #return render(request, 'pedidos.html',context)
+
+class PedidosListView(ListView):
+    model = Pedido
+    template_name ='pedidos.html'
+    queryset = Pedido.objects.order_by('entregado')
+    context_object_name ='lista_pedidos'
+
+    def get_context_data(self, **kwargs):
+        context= super(PedidosListView, self).get_context_data(**kwargs)
+        context['Titulo_pagina']='Listado de pedidos'
+        return context
+
+#DEVUELVE LOS DATOS DE CADA PEDIDO
+
+#pedidos = Pedido.objects.get(pk=pedido_id)
 	#output = ', '.join([pedidos.identificador, str(pedidos.cliente)])
 	#return HttpResponse(output)
-    pedidos= get_object_or_404(Pedido,pk=pedido_id)
-    context= {'pedido': pedidos}
-    return render(request, 'Datos_Pedido.html', context)
+
+#def detalle_pedidos(request, pedido_id):
+    #pedidos= get_object_or_404(Pedido,pk=pedido_id)
+    #context= {'pedido': pedidos}
+    #return render(request, 'Datos_Pedido.html', context)
+
+class Detalle_PedidosDetailView(DetailView):
+    model = Pedido
+    template_name = 'Datos_Pedido.html'
+
+    def get_context_data(self, **kwargs):
+        context= super(Detalle_PedidosDetailView, self).get_context_data(**kwargs)
+        context['Titulo_pagina']= 'Datos del pedido'
+        return context
