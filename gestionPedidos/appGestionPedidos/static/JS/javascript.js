478dc95dbfe143b89df754aa2e739ab07b955d91
Unknown <alvaro.husillos@opendeusto.es>
2020-05-14 13:33:15 +0200

    Se muestra "Entregado" y "Pendiente" en lugar de "True" o "False"

--- gestionPedidos/appGestionPedidos/static/JS/javascript.js
+++ gestionPedidos/appGestionPedidos/static/JS/javascript.js
@@ -1,3 +1,4 @@
+//Cálculo del precio total en el formulario del pedido
 function calcularPrecio(){
 
     let base_imponible = document.getElementById("id_base_imponible").value;
@@ -5,5 +6,17 @@ function calcularPrecio(){
     document.getElementById("id_precio").value = base_imponible*(1+ iva/100);
 
 }
-let precio = document.getElementById("id_precio");
-precio.onclick = calcularPrecio;
+document.getElementById("id_precio").onclick = calcularPrecio;
+
+//Vamos a mostrar al usuario si el pedido esta "Pendiente" o "Entregado" en lugar de "True" o "False"
+function cambiarEstado(){
+    let estado = document.getElementsByClassName("tercera");
+    for(let i = 0;i<estado.length; i++){
+        if (estado[i].textContent == "False"){
+        estado[i].textContent = "Pendiente";
+        }else if (estado[i].textContent == "True"){
+            estado[i].textContent = "Entregado";
+        }
+    }
+}
+
