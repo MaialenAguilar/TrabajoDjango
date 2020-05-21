54fc9b95344cc16804ace10ef0e28d7d3800ea33
Unknown <alvaro.husillos@opendeusto.es>
2020-05-21 10:58:31 +0200

    Mostrar mensaje de guardado satisfactorio y poner formulario en blanco

--- gestionPedidos/appGestionPedidos/static/JS/Form.js
+++ gestionPedidos/appGestionPedidos/static/JS/Form.js
@@ -57,4 +57,26 @@ function limpiarFormulario(){
 let guardar = document.getElementById('guardar');
 guardar.addEventListener('click', limpiarFormulario);
 
+function crearMensaje(){
+    let mensaje = `
+        <div class="mensaje-close" id="mensaje-close" onclick="cerrarPopup()">x</div>
+        <p id="contenido-mensaje">El cliente ha sido registrado correctamente</p>
+        `;
+     return mensaje;
+}
+function cerrarPopup(){
+    let popup = document.getElementById('mensaje-content');
+
+    popup.style.display = 'none';
+
+}
+function mostrarMensaje(){
+    //Seleccionamos el div que hemos creado
+    let div = document.getElementById('mensaje-content');
+    let mensaje = crearMensaje(); // Devuelve el mensaje para insertar en el DIV
+    div.innerHTML=''; //Borramos el contenido del div por si tuviera algo
+    div.innerHTML = mensaje; //Insertamos el mensaje en el div
+}
+guardar.addEventListener('click', mostrarMensaje);
+
 
